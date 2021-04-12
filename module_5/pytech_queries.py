ef9e8173776474033d43b71a9914a4de44207cb9
Chevant Starr <cmstarr@m635.bellevue.edu>
2021-04-11 20:39:44 -0700

    Nicer output to programs

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -5,10 +5,12 @@ db = client.pytech
 docs = db.students.find({})
 print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 for doc in docs:
-    print(f"Student ID {doc['student_id']}")
-    print(f"First Name {doc['first_name']}")
-    print(f"Student ID {doc['last_name']}\n")
+    print(f"Student ID: {doc['student_id']}")
+    print(f"First Name: {doc['first_name']}")
+    print(f"Last Name: {doc['last_name']}\n")
 
 doc = db.students.find_one({"student_id": "1007"})
-print(doc["last_name"])
-print(f"Student ID {doc['student_id']}")
+print("-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print(f"Student ID: {doc['student_id']}")
+print(f"First Name: {doc['first_name']}")
+print(f"Last Name: {doc['last_name']}\n")
