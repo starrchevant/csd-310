5f11e4c121eb74df0e97f34f3bb247302aa0dfa0
Chevant Starr <cmstarr@m635.bellevue.edu>
2021-04-29 11:56:02 -0700

    this is just a save point

---
+++ module_9/pysports_update_and_delete.py
@@ -0,0 +1,44 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+def printallstudents(cursor):
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
+    players = cursor.fetchall()
+    print("-- DISPLAYING PLAYER RECORDS --")
+    for player in players:
+        print("Player ID: {}".format(player[0]))
+        print("First Name: {}".format(player[1]))
+        print("Last Name: {}".format(player[2]))
+        print("Team Name: {}".format(player[3]))
+        print()
+
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO player (first_name, last_name, team_id) VALUES('Smeagol', 'Shire Folk', 1)")
+    db.commit()
+    printallstudents(cursor)
+    cursor.execute()
+    db.commit
+    input("Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+finally:
+    db.close()
