88a22ad91fb5bb8acd2b0cd12c039d632f2dfabc
Chevant Starr <cmstarr@m635.bellevue.edu>
2021-04-30 11:02:31 -0700

    moudule_9.3

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -12,7 +12,7 @@ config = {
 def printallstudents(cursor):
     cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
     players = cursor.fetchall()
-    print("-- DISPLAYING PLAYER RECORDS --")
+    #print("-- DISPLAYING PLAYER RECORDS --")
     for player in players:
         print("Player ID: {}".format(player[0]))
         print("First Name: {}".format(player[1]))
@@ -26,9 +26,16 @@ try:
     cursor = db.cursor()
     cursor.execute("INSERT INTO player (first_name, last_name, team_id) VALUES('Smeagol', 'Shire Folk', 1)")
     db.commit()
+    print("-- DISPLAYING PLAYER AFTER INSERT --")
     printallstudents(cursor)
-    cursor.execute()
-    db.commit
+    cursor.execute("UPDATE player SET team_id = 2, first_name = 'Gollum', last_name = 'Ring Stealer' WHERE first_name = 'Smeagol' ");
+    db.commit()
+    print("-- DISPLAYING PLAYER AFTER UPDATE --")
+    printallstudents(cursor)
+    cursor.execute("DELETE FROM player WHERE first_name = 'Gollum'");
+    db.commit()
+    print("-- DISPLAYING PLAYER AFTER DELETE --")
+    printallstudents(cursor)
     input("Press any key to continue...")
 
 except mysql.connector.Error as err:
